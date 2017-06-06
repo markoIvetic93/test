d78f41640da62a2ad2060274af483ca634cab927
Bosko Sogura <bolesogura93@gmail.com>
2017-06-06 21:32:02 +0200

    added user profile entity and configured database

---
+++ app/Bootstrap.java
@@ -0,0 +1,15 @@
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
+
+@OnApplicationStart
+public class Bootstrap extends Job{
+	public void doJob() {
+		if (Parking.count()== 0){
+			Fixtures.loadModels("initial-data.yml");
+			System.out.println("Database populated with initial data.");
+		}
+	}
+}
