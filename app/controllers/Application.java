d125894f6447b91c75d3cd376d19c8b761d536cd
Bosko Sogura <bolesogura93@gmail.com>
2017-06-11 13:54:18 +0200

    Application controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,21 +5,12 @@ import play.mvc.*;
 
 import java.util.*;
 
-import models.MyUser;
+import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
-    }
-    
-    public static void users() {
-    	try{
-        List<MyUser> users = MyUser.findAll();
-        renderJSON(users);
-    	}catch(Exception e){
-			error("Unable to read entities!");
-		}
     }
 
 }
