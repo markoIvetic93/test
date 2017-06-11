f261dffc78de712f07bd55f75b8a9b56a8ba23da
Bosko Sogura <bolesogura93@gmail.com>
2017-06-11 13:51:57 +0200

    User and Parking controller and models

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,13 +1,12 @@
-import play.*;
-import play.jobs.*;
-import play.test.*;
-
-import models.*;
+import models.Parking;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
 
 @OnApplicationStart
 public class Bootstrap extends Job{
 	public void doJob() {
-		if (MyUser.count()== 0){
+		if (Parking.count()== 0){
 			Fixtures.loadModels("initial-data.yml");
 			System.out.println("Database populated with initial data.");
 		}
