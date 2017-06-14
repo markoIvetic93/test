6b5974c296cee24aadce2a015882f9442343f115
Milana Dapra <milana.dapra@gmail.com>
2017-06-14 19:38:03 +0200

    izmjena

--- app/controllers/Parkings.java
+++ app/controllers/Parkings.java
@@ -30,12 +30,18 @@ public class Parkings extends Controller{
 	
 	public static void rate(){
 		try{
-			String title =  params.get("body");
-			int rating = 2;
+			String jsonAsString = params.get("body");
+			JsonParser jsonParser = new JsonParser();
+			JsonObject jsonObject = (JsonObject)jsonParser.parse(jsonAsString);
+			models.Parking newParking = new Gson().fromJson(jsonObject, models.Parking.class);
+			
+			String title =  newParking.parkingName;
+			int rating = newParking.ratingSum;
 			List<models.Parking> parkings = models.Parking.find("byParkingname", title).fetch();
 			models.Parking parking = parkings.get(0);
 			parking.ratingSum = parking.ratingSum + rating;
 			parking.numberOfVotes = parking.numberOfVotes + 1;
+			parking.save();
 			renderJSON(parking);
 		}catch(Exception e){
 			error("Unable to read entity!");
